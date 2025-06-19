e4ce15df7bd228b313f6fd2f0506e6502fbc9df2
Miles J. Litteral <Mandaloe2@gmail.com>
2025-06-19 10:05:22 -0400

    Update RubyScript.cs

--- Assets/RubyScript.cs
+++ Assets/RubyScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class RubyScript : MonoBehaviour {
 
-    public String filePath;
+    public string filePath;
     
     // Use this for initialization
     void Start () {
